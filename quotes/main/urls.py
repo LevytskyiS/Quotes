20470605eafc1e9a623867c3d75ebd9c64db1084
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-12-20 15:29:54 +0100

    added pk, app_name 'main:index' etc

--- quotes/main/urls.py
+++ quotes/main/urls.py
@@ -2,8 +2,27 @@ from django.urls import path
 
 from . import views
 
+# Чтобы избежать путаницы с ссылками, то перед ними можно ставить название приложение
+# main:index
+app_name = "main"
 urlpatterns = [
     path("", views.QuoteListView.as_view(), name="index"),
+    path(
+        "author/<int:author_id>/",
+        # "author/<slug:first_name>/",
+        views.AuthorDetailView.as_view(),
+        name="author_detail",
+    ),
+    path(
+        "quote/<int:quote_id>/",
+        # "author/<slug:first_name>/",
+        views.QuoteDetailView.as_view(),
+        name="quote_detail",
+    ),
     path("add_author/", views.CreateAuthor.as_view(), name="add_author"),
     path("add_quote/", views.CreateQuote.as_view(), name="add_quote"),
 ]
+
+# AuthorDetailView
+# <a href="{% url 'author_detail' quote.author.first_name %}">{{ quote.author }}</a>
+# for "author/<slug:first_name>/"
--- quotes/main/views.py
+++ quotes/main/views.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from .models import Author, Quote
 from .forms import AuthorForm, QuoteForm
@@ -19,11 +19,32 @@ class QuoteListView(ListView):
         return super().get_queryset()
 
 
+class AuthorDetailView(DetailView):
+    model = Author
+    # По умолчанию Django ищет шаблон в каталоге <application_name>/<model_name>_detail.html, т.е. blog/post_detail.html
+    template_name = "main/author_detail.html"
+    # AttributeError is raised if pk is None and slug is None
+    pk_url_kwarg = "author_id"
+    # OR
+    # slug_url_kwarg = "first_name"
+    # slug_field = "first_name"
+
+
+class QuoteDetailView(DetailView):
+    model = Quote
+    template_name = "main/quote_detail.html"
+    pk_url_kwarg = "quote_id"
+
+
 class CreateAuthor(CreateView):
     model = Author
     form_class = AuthorForm
     template_name = "main/add_author.html"
     success_url = "/"
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        print(form)
+        return super().form_valid(form)
 
 
 class CreateQuote(CreateView):
