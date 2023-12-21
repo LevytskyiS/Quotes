32354e92aa22d3135a74821c4d89ab31e9cd235d
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-12-21 18:21:07 +0100

    implemented 'favourite quotes' functionality

--- quotes/main/static/main/js/index.js
+++ quotes/main/static/main/js/index.js
@@ -1,34 +1,42 @@
-const voteBtn = document.querySelector(".js-vote-up");
+// favourite function
+const favouriteQuote = document.querySelector(".js-quote-list");
 
-voteBtn.addEventListener("click", onClickVote);
+favouriteQuote.addEventListener("click", onClickFavouriteQuote);
 
-function onClickVote(evt) {
-  const { target } = evt;
-  const quoteId = document.querySelector(".js-quote-vote").dataset.quoteId
-  console.log(quoteId)
-  if (target.value === "submit") {
-    const value = 1;
-    passData(value, quoteId)
-    console.log("Passed a vote")
+function onClickFavouriteQuote(evt) {
+  if (
+    evt.target.style.color === "gold" &&
+    evt.target.classList.contains("js-quote-favourite")
+  ) {
+    evt.target.style = "";
+    const quoteId = evt.target.closest("li").dataset.quoteId;
+    localStorage.removeItem(`quote_${quoteId}`);
+    return;
+  }
+
+  if (evt.target.classList.contains("js-quote-favourite")) {
+    evt.target.style.color = "gold";
+    const quoteId = evt.target.closest("li").dataset.quoteId;
+    localStorage.setItem(
+      `quote_${quoteId}`,
+      JSON.stringify({ quoteId: quoteId })
+    );
+    return;
   }
 }
 
-function passData(value, quoteId) {
-    let url = "/vote_up/"
+const quotesFavourite = document.querySelectorAll(".js-li-favourite");
 
-    fetch(url, {
-        method: "POST",
-        headers:{
-            "Content-Type":"application/json",
-            "X-CSRFToken": csrftoken,
-        },
-        body:JSON.stringify({"value": value, "quoteId": quoteId})
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log("Data: ", data)
-        location.reload()
-    })
+function getQuoteId() {
+  for (let i = 0; i < quotesFavourite.length; i++) {
+    const dataQuoteId = quotesFavourite[i].dataset.quoteId;
+    const storageQuoteId = localStorage.getItem(`quote_${dataQuoteId}`);
+    if (storageQuoteId) {
+      const quoteId = JSON.parse(storageQuoteId).quoteId;
+      const goldDiv = quotesFavourite[i].querySelector(".js-quote-favourite");
+      goldDiv.style.color = "gold";
+    }
+  }
 }
+
+getQuoteId();
